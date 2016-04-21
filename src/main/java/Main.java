3b84a55bbf4b36d99afdaa8d20a504af2367c18a
Vasyl Vavrychuk <vvavrychuk@gmail.com>
2016-04-21 15:14:57 +0300

    add options for running on Heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,29 +12,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.print("Enter search query: ");
-        String query = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+  public static void main(String[] args) throws IOException, InterruptedException {
+    System.out.print("Enter search query: ");
+    String query = (new BufferedReader(new InputStreamReader(System.in))).readLine();
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com");
+    ChromeDriverService service = XvfbChromeDriverService.createDefaultService();
 
-        // Enter query into HTML input.
-        WebElement queryInput = driver.findElement(By.name("q"));
-        queryInput.sendKeys(query);
-        queryInput.submit();
+    ChromeOptions options = new ChromeOptions();
+    options.setBinary("/app/.apt/opt/google/chrome/chrome");
+    options.addArguments("--no-sandbox");
 
-        // Google loads search results are loaded asynchronously with JavaScript.
-        // Wait for this to happen.
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement searchResults = wait.until(
-                ExpectedConditions.presenceOfElementLocated(By.id("rso")));
+    WebDriver driver = new ChromeDriver(service, options);
+    driver.get("http://www.google.com");
 
-        for (WebElement searchResult : searchResults.findElements(By.tagName("h3"))) {
-            System.out.println(searchResult.getText());
-        }
+    // Enter query into HTML input.
+    WebElement queryInput = driver.findElement(By.name("q"));
+    queryInput.sendKeys(query);
+    queryInput.submit();
 
-        Thread.sleep(1000);
-        driver.quit();
+    // Google loads search results are loaded asynchronously with JavaScript.
+    // Wait for this to happen.
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    WebElement searchResults = wait.until(
+      ExpectedConditions.presenceOfElementLocated(By.id("rso")));
+
+    for (WebElement searchResult : searchResults.findElements(By.tagName("h3"))) {
+      System.out.println(searchResult.getText());
     }
+
+    Thread.sleep(1000);
+    driver.quit();
+  }
 }
---
+++ src/main/java/XvfbChromeDriverService.java
@@ -0,0 +1,47 @@
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import java.io.File;
+import java.io.IOException;
+
+class XvfbChromeDriverService extends ChromeDriverService {
+  XvfbChromeDriverService(File executable, int port, ImmutableList<String> args, ImmutableMap<String, String> environment) throws IOException {
+    super(executable, port, args, environment);
+  }
+
+  public static ChromeDriverService createDefaultService() {
+    return new Builder().usingAnyFreePort().build();
+  }
+
+  public static class Builder extends ChromeDriverService.Builder {
+    @Override
+    protected File findDefaultExecutable() {
+      return findExecutable("chromedriver", "", null, null);
+    }
+
+    @Override
+    protected ImmutableList<String> createArgs() {
+      try {
+        return ImmutableList.<String>builder()
+          .add(super.findDefaultExecutable().getCanonicalPath())
+          .addAll(super.createArgs())
+          .build();
+      } catch (IOException e) {
+        throw new WebDriverException(e);
+      }
+    }
+
+    @Override
+    protected ChromeDriverService createDriverService(File exe, int port,
+                                                      ImmutableList<String> args,
+                                                      ImmutableMap<String, String> environment) {
+      try {
+        return new XvfbChromeDriverService(exe, port, args, environment);
+      } catch (IOException e) {
+        throw new WebDriverException(e);
+      }
+    }
+  }
+}
